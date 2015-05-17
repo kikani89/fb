8438689eb90718a24f92c7c9c4831911e1cceb57
kikani89@yahoo.com <kikani89@yahoo.com>
2015-05-17 10:12:00 +0530

    fb_login

--- lib/include.php
+++ lib/include.php
@@ -7,7 +7,7 @@ ini_set('max_execution_time', 300);
 $fb_app_id = '1666410266911684';
 $fb_secret_id = '06090170170002699844c87d1b5b40cc';
 
-$fb_login_url = 'http://localhost/xampp/isuworkspace/fb/src/index.php';
+$fb_login_url = 'http://facebookchallenge-ifa.rhcloud.com/src/index.php';
 echo "hello";
 require_once ('Facebook/autoload.php');
 /**
