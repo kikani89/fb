861e6dc92dfb4164da9705b24203c9ebaffe4a41
kikani89@yahoo.com <kikani89@yahoo.com>
2016-07-08 20:18:39 +0530

    checking rhs

--- lib/include.php
+++ lib/include.php
@@ -2,7 +2,6 @@
 ini_set('max_execution_time', 300);
 /**
  * it set the application id and application secret id
- *
  */
 $fb_app_id = '1666410266911684';
 $fb_secret_id = '06090170170002699844c87d1b5b40cc';
