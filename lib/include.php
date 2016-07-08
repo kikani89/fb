49f7b1c212980f58d1a805d6351b546348f416e4
kikani89@yahoo.com <kikani89@yahoo.com>
2016-07-08 19:15:37 +0530

    rhs cloud update

--- lib/include.php
+++ lib/include.php
@@ -4,10 +4,10 @@ ini_set('max_execution_time', 300);
  * it set the application id and application secret id
  *
  */
-$fb_app_id = '1411669032494286';
-$fb_secret_id = 'd40aa6edddcb904dab949e406324b9ba';
+$fb_app_id = '1666410266911684';
+$fb_secret_id = '06090170170002699844c87d1b5b40cc';
 
-$fb_login_url = 'http://localhost/xampp/fb/fb/src/index.php';
+$fb_login_url = 'http://phpfb-ifa.rhcloud.com/src/index.php';
 
 require_once ('Facebook/autoload.php');
 /**
