ec43e81c153d8b1a8abaedd96d38dba1eadee2a9
kikani89@yahoo.com <kikani89@yahoo.com>
2016-07-08 19:11:27 +0530

    progressbar update

--- src/index.php
+++ src/index.php
@@ -23,7 +23,7 @@
  * it set the all configuration for the facebook app
  */
 
-include_once ("/lib/include.php");
+include_once ("../lib/include.php");
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
@@ -82,10 +82,10 @@ try {
 						</div>
 					</div>
 				</div>
-				<div id="progressbar" class="progress hide">
-					<div id="progressbar1" class="progress-bar" role="progressbar"
-						aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"
-						style="width: 1%;"></div>
+				<div class=" progress-container" >
+    				<div class="progress progress-striped active">
+        				<div class="progress-bar progress-bar-success" style="width:5%"></div>
+    				</div>
 				</div>
 				<nav class=" navbar navbar-inverse">
 				<div class="navbar-header">
@@ -138,9 +138,9 @@ try {
 						<div class="form-control input-group" style="margin-left: 2%;">
 							<span class="input-group-addon input-sm"> <input
 								class="select-album" type="checkbox"
-								value="<?php echo $album['id'].','.$album['name'];?>" />
+								value="<?php echo $album['id'] . ',' . $album['name']; ?>" />
 							</span>
-							<button rel="<?php echo $album['id'].','.$album['name'];?>"
+							<button rel="<?php echo $album['id'] . ',' . $album['name']; ?>"
 								class="single-download btn btn-default btn-sm"
 								title="Download Album">
 								<span class="glyphicon glyphicon-save" aria-hidden="true"></span>
@@ -152,11 +152,10 @@ try {
 						</div>
 					</div>
                       <?php
-				}
-			}
-		}
-	} else {
-		
+					}
+					}
+					}
+					} else {
 		?>        
             <div class="container"
 						style="margin-top: 5px; padding-top: 0px">
@@ -165,7 +164,7 @@ try {
 							<ul class="nav nav-pills pull-right">
 
 								<li class="active"><a
-									href="<?php echo $helper->getLoginUrl (array("user_photos","public_profile"));?>">Login</a></li>
+									href="<?php echo $helper -> getLoginUrl(array("user_photos", "public_profile")); ?>">Login</a></li>
 							</ul>
 							</nav>
 							<h2 class="text-muted">Wel-Come To Facebook Challenge</h2>
@@ -174,65 +173,103 @@ try {
 							<hr>
 					
 					</div><?php
-	}
-} catch ( Exception $ex ) {
-	echo $ex;
-}
-?>
+					}
+					} catch ( Exception $ex ) {
+					echo $ex;
+					}
+				?>
   </div>
 
 </body>
 <script type="text/javascript">
-	
+	$(".progress-container").hide();
+	function showProgress() {
+		$( ".progress-bar" ).css( "width", "5%" ).attr( "aria-valuenow", 0);
+		var $progressBar = $('.progress-bar');
+		$(".progress-container").show();
+		setTimeout(function() {
+			$progressBar.css('width', '10%');
+			setTimeout(function() {
+				$progressBar.css('width', '30%');
+				setTimeout(function() {
+					$progressBar.css('width', '50%');
+					setTimeout(function() {
+						$progressBar.css('width', '70%');
+						setTimeout(function() {
+							$progressBar.css('width', '80%');
+						});
+					}, 1000);
+					// WAIT 5 milliseconds
+				}, 3000);
+				// WAIT 2 seconds
+			}, 3000);
+			// WAIT 1 seconds
+		}, 5000);
+	}
+
 	function append_download_link(url) {
-					$.ajax({
-						url:url,
-						success:function(result){
-							$("#display-response").html(result);
-							$("#download-modal").modal({
-								show: true
-							});
-						}
-					});
-				}
-    
+
+		$.ajax({
+			url : url,
+			success : function(result) {
+				$(".progress-bar").animate({
+					width : "100%"
+				}, 500);
+
+				$("#display-response").html(result);
+				$("#download-modal").modal({
+					show : true
+
+				});
+				setTimeout(function(){
+				$(".progress-container").hide();
+				//$(".progress-Bar").css('width', '10%');	
+				
+				},2000);
+				
+			}
+		});
+	}
+
+
 	$("#download-all-albums").on("click", function() {
-        append_download_link("../lib/download_album.php?zip=1&all_albums=all_albums");
+		showProgress();
+		append_download_link("../lib/download_album.php?zip=1&all_albums=all_albums");
 
 	});
-//single download
-    				$(".single-download").on("click", function() {
-        
-					var rel = $(this).attr("rel");
-					var album = rel.split(",");
+	//single download
+	$(".single-download").on("click", function() {
+		showProgress();
+		var rel = $(this).attr("rel");
+		var album = rel.split(",");
 
-					append_download_link("../lib/download_album.php?zip=1&single_album="+album[0]+","+album[1]);
-				});
+		append_download_link("../lib/download_album.php?zip=1&single_album=" + album[0] + "," + album[1]);
+	});
 
-    
-    //get selected data
-    
-    function get_all_selected_albums() {
-        
-					var selected_albums;
-					var i = 0;
-					$(".select-album").each(function () {
-						if ($(this).is(":checked")) {
-							if (!selected_albums) {
-								selected_albums = $(this).val();
-							} else {
-								selected_albums = selected_albums + "/" + $(this).val();
-							}
-						}
-					});
+	//get selected data
 
-					return selected_albums;
+	function get_all_selected_albums() {
+		showProgress();
+		var selected_albums;
+		var i = 0;
+		$(".select-album").each(function() {
+			if ($(this).is(":checked")) {
+				if (!selected_albums) {
+					selected_albums = $(this).val();
+				} else {
+					selected_albums = selected_albums + "/" + $(this).val();
 				}
-//selected data
-    $("#download-selected-albums").on("click", function() {
-        
-					var selected_albums = get_all_selected_albums();
-					append_download_link("../lib/download_album.php?zip=1&selected_albums="+selected_albums);
-				});
+			}
+		});
+
+		return selected_albums;
+	}
+
+	//selected data
+	$("#download-selected-albums").on("click", function() {
+		showProgress();
+		var selected_albums = get_all_selected_albums();
+		append_download_link("../lib/download_album.php?zip=1&selected_albums=" + selected_albums);
+	}); 
 </script>
 </html>
